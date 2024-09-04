b747c275b645a2a8e4a502c299380655f1e93d15
MikeHE-CODER <a1918791@adelaide.edu.au>
2024-09-04 14:23:52 +0930

    Update main-1-1.cpp

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -1,17 +1,15 @@
 #include "AirCraft.h"
-#include "AirCraft.cpp"
 #include <iostream>
 using namespace std;
-int main(){
-    AirCraft a;
-    AirCraft b(3);
-    a.set_fuel(0.4);
-    a.set_numberOfFlights(3);
-    a.set_weight(100);
-    a.fly(3,4);
-    a.get_fuel();
-    a.get_numberOfFlights();
-    a.get_weight();
-    a.refuel();
-
+int main() {
+  AirCraft a;
+  AirCraft b(3);
+  a.set_fuel(0.4);
+  a.set_numberOfFlights(3);
+  a.set_weight(100);
+  a.fly(3, 4);
+  a.get_fuel();
+  a.get_numberOfFlights();
+  a.get_weight();
+  a.refuel();
 }
