b9b9d911db7f8abae7e32ecd8f3b83f7aa144e1b
MikeHE-CODER <a1918791@adelaide.edu.au>
2024-09-04 14:23:35 +0930

    Update AirCraft.h

--- AirCraft.h
+++ AirCraft.h
@@ -2,21 +2,23 @@
 #define AirCraft_h
 #include <iostream>
 using namespace std;
-class AirCraft{
+class AirCraft {
 public:
-AirCraft();
-AirCraft(int w);             // creates an Aircraft with weight w
-void refuel();               // Resets fuel back to 100%
-virtual void fly(int headwind, int minutes); // headwind in km/h and minutes (time flying)
-void set_weight(int weight);
-void set_fuel(float fuel);
-void set_numberOfFlights(int numberOfFlights);
-int get_weight();
-float get_fuel();
-int get_numberOfFlights();
+  AirCraft();
+  AirCraft(int w); // creates an Aircraft with weight w
+  void refuel();   // Resets fuel back to 100%
+  virtual void fly(int headwind,
+                   int minutes); // headwind in km/h and minutes (time flying)
+  void set_weight(int weight);
+  void set_fuel(float fuel);
+  void set_numberOfFlights(int numberOfFlights);
+  int get_weight();
+  float get_fuel();
+  int get_numberOfFlights();
+
 private:
-int weight;                  // the weight of AirCraft
-float fuel=1;                  // fuel percentage, initially 100%
-int numberOfFlights=0;        // initially 0
+  int weight;              // the weight of AirCraft
+  float fuel = 1;          // fuel percentage, initially 100%
+  int numberOfFlights = 0; // initially 0
 };
 #endif
