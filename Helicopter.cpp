868c067a6e438efb2bc339370dc8b32fe3511215
MikeHE-CODER <a1918791@adelaide.edu.au>
2024-09-04 14:32:39 +0930

    Update Helicopter.cpp

--- Helicopter.cpp
+++ Helicopter.cpp
@@ -1,23 +1,26 @@
 #include "Helicopter.h"
+#include "AirCraft.h"
 #include <iostream>
 #include <string.h>
 using namespace std;
-Helicopter::Helicopter(int w, string n) :AirCraft(w),name(n){}    // creates a Helicopter with weight w and name n
-string Helicopter::get_name(){return name;};     
-void Helicopter::set_name(string a){this->name=a;};            
-void Helicopter::fly(int headwind, int minutes){
-    float chushi=get_fuel;
-    if (headwind<40){
-        this->set_fuel(get_fuel-=0.002*minutes);
-    }else{
-        this->set_fuel(get_fuel-=0.004*minutes);
-    }
-    if (this->get_weight>5670){
-        this->get_weight-=0.0001*minutes;
-    }
-    if (this->get_fuel<0.2){
-        this->set_fuel(chushi);
-    }else{
-        this->set_numberOfFlights(this->get_numberOfFlights++);
-    }
+Helicopter::Helicopter(int w, string n) : name(n) {
+  this->set_weight(w);
+} // creates a Helicopter with weight w and name n
+string Helicopter::get_name() { return name; };
+void Helicopter::set_name(string a) { this->name = a; };
+void Helicopter::fly(int headwind, int minutes) {
+  float chushi = get_fuel();
+  if (headwind < 40) {
+    this->set_fuel(this->get_fuel() - 0.002 * minutes);
+  } else {
+    this->set_fuel((this->get_fuel() - 0.004 * minutes);
+  }
+  if (this->get_weight() > 5670) {
+    this->set_weight(this->get_fuel()-0.0001 * minutes;
+  }
+  if (this->get_fuel() < 0.2) {
+    this->set_fuel(chushi);
+  } else {
+    this->set_numberOfFlights(this->get_numberOfFlights() + 1);
+  }
 }
