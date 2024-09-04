93fd04b34af5f7081aab98e373b8217bfb2a4667
MikeHE-CODER <a1918791@adelaide.edu.au>
2024-09-04 13:30:52 +0930

    Create main-1-1.cpp

---
+++ main-1-1.cpp
@@ -0,0 +1,15 @@
+#include "AirCraft.h"
+#include <iostream>
+int main(){
+    AirCraft a;
+    AirCraft b(3);
+    a.set_fuel(0.4);
+    a.set_numberOfFlights(3);
+    a.set_weight(100);
+    a.fly(3,4);
+    a.get_fuel;
+    a.get_numberOfFlights;
+    a.get_weight;
+    a.refuel;
+
+}
