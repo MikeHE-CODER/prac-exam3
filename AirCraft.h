9025ec69a17b8e26b845da98a36e24b84a130ed7
MikeHE-CODER <a1918791@adelaide.edu.au>
2024-09-04 13:30:03 +0930

    Create AirCraft.h

---
+++ AirCraft.h
@@ -0,0 +1,22 @@
+#ifndef AirCraft_h
+#define AirCraft_h
+#include <iostream>
+
+class AirCraft{
+public:
+AirCraft();
+AirCraft(int w);             // creates an Aircraft with weight w
+void refuel();               // Resets fuel back to 100%
+virtual void fly(int headwind, int minutes); // headwind in km/h and minutes (time flying)
+void set_weight(int weight);
+void set_fuel(float fuel);
+void set_numberOfFlights(int numberOfFlights);
+int get_weight();
+float get_fuel();
+int get_numberOfFlights();
+private:
+int weight;                  // the weight of AirCraft
+float fuel=1;                  // fuel percentage, initially 100%
+int numberOfFlights=0;         // initially 0
+}
+#endif
