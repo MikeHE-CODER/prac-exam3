ef5430b2a5f9eed2b1b1920a8080866e1dc23ada
MikeHE-CODER <a1918791@adelaide.edu.au>
2024-09-04 14:00:10 +0930

    Create main-1-2.cpp

---
+++ main-1-2.cpp
@@ -0,0 +1,11 @@
+#include "AirCraft.cpp"
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main(){
+    Helicopter a(3,"fadf");
+    a.set_name("fdafff");
+    a.get_name;
+    a.fly(3,5);
+}
