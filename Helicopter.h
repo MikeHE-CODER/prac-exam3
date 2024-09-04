ad311c88de5cffa904b09b690ccd58307c7e10b0
MikeHE-CODER <a1918791@adelaide.edu.au>
2024-09-04 14:13:19 +0930

    Update Helicopter.h

--- Helicopter.h
+++ Helicopter.h
@@ -5,9 +5,9 @@ using namespace std;
 #include <string.h>
 class Helicopter:public AirCraft{
 public:
-Helicopter(int w, string n)     // creates a Helicopter with weight w and name n
+Helicopter(int w, string n) ;    // creates a Helicopter with weight w and name n
 string get_name();   
-void set_name(string a)     
+void set_name(string a) ;    
 void fly(int headwind, int minutes);
 private:
 string name;  // name of the Helicopter
